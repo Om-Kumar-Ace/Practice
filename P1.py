ec2d194296e8c8c9a77d556479aef6b7b46b045e
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-01-24 01:00:09 +0530

    New

---
+++ P1.py
@@ -0,0 +1,5 @@
+#Accept two words as input and print the two words after adding a space between them.
+word1 = input("Enter the first word: ")
+word2 = input("Enter the second word: ")
+print(word1 + ' ' + word2)
+

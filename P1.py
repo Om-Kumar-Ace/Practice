0884b868c62d9b811291782c7b1e602776a1e506
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-01-24 01:05:10 +0530

    New

--- P1.py
+++ P1.py
@@ -3,3 +3,7 @@ word1 = input("Enter the first word: ")
 word2 = input("Enter the second word: ")
 print(word1 + ' ' + word2)
 
+#Accept the registration number of a vehicle as input and print its state-code as output.
+registration_number = input("Enter the vehicle registration number: ")
+state_code = registration_number[:2]
+print(state_code)

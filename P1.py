7ea31cbf9bddd1359c01af1ca5cd79a7d178f05f
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-01-24 19:33:21 +0530

    New

--- P1.py
+++ P1.py
@@ -53,3 +53,13 @@ def is_criminal_plate(plate):
   
 plate =input("")
 print(is_criminal_plate(plate))
+
+#Accept five words as input and print the sentence formed by these words after adding a space between consecutive words and a full stop at the end.
+word1 = input(" ")
+word2 = input(" ")
+word3 = input(" ")
+word4 = input(" ")
+word5 = input("")
+
+sentence = f"{word1} {word2} {word3} {word4} {word5}."
+print(sentence)

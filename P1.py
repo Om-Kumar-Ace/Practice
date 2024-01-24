cf1b807b9ebc553fd1c27ab4e8dd75ec5890aa24
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-01-24 18:40:44 +0530

    New

--- P1.py
+++ P1.py
@@ -21,12 +21,9 @@ else:
 #1 2 3 2 1
 #1 2 3 4 3 2 1
 #1 2 3 4 5 4 3 2 1
-def print_pattern(rows):
-    for i in range(1, rows + 1):
-        for j in range(1, i + 1):
-            print(j, end=" ")
-        for j in range(i - 1, 0, -1):
-            print(j, end=" ")
-        print()
-
-print_pattern(4)
+for i in range(1, 5):
+  for j in range(1, i + 2):
+    print(j, end=" ")
+  for j in range(i, 0, -1):
+    print(j, end=" ")
+  print()

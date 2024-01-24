7e253184f39f5892bcdce52781dc38e1961d73e8
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-01-24 18:32:12 +0530

    New

--- P1.py
+++ P1.py
@@ -15,3 +15,18 @@ if len(number) != 5 or not number.isdigit():
 else:
     digit_sum = sum(int(digit) for digit in number)
     print("Sum of digits:", digit_sum)
+
+#Print the following pattern. There is exactly one space between any two consecutive numbers on any line. There are no spaces at the end of any line.
+#1 2 1
+#1 2 3 2 1
+#1 2 3 4 3 2 1
+#1 2 3 4 5 4 3 2 1
+def print_pattern(rows):
+    for i in range(1, rows + 1):
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        for j in range(i - 1, 0, -1):
+            print(j, end=" ")
+        print()
+
+print_pattern(4)

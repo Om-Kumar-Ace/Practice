b711d94baa154d62ae121564b7d1d5948000dedb
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-01-24 19:30:07 +0530

    New

--- P1.py
+++ P1.py
@@ -40,3 +40,16 @@ if is_data_science:
     print("True")
 else:
     print("False")
+
+#The police are trying to track a criminal based on the evidence available at a crime site. Their main clue is a vehicle's damaged number plate. Only the string TN07 is visible. The format of the registration number is AA00AA00, where the first two letters are alphabets, next two are numbers, next two are again alphabets followed by two numbers at the end. A number plate is picked from a database of registration numbers and is given to you as input. Your task is to determine if this could belong to the criminal or not. Print True if the number plate contains TN07 and False otherwise.
+def is_criminal_plate(plate):
+
+  occurrences = [i for i in range(len(plate)) if plate.find("TN07", i) == i]
+
+  for i in occurrences:
+    if "TN07" in  plate[i:i + 9]  :
+      return True
+  return False
+  
+plate =input("")
+print(is_criminal_plate(plate))

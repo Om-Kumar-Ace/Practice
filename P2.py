d22d0302cc8f4af582ca2f096304c8bc0899be24
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-01-26 19:49:31 +0530

    New

--- P2.py
+++ P2.py
@@ -1,1 +1,7 @@
 #Print positive if it is greater than zero and negative if it is less than zero. You can assume that the input will be non-zero.
+X = float(input(""))
+
+if X > 0:
+    print("positive")
+else:
+    print("negative")

d664685c7c100c510dab2dab68f8ba624ca5f036
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-02-02 17:47:24 +0530

    New

--- P2.py
+++ P2.py
@@ -53,3 +53,15 @@ print(younger_person)
 
 #Accept a string as input. Your task is to determine if the input string is a valid password or not. For a string to be a valid password, it must satisfy all the conditions given below: (1) It should have at least 8 and at most 32 characters (2) It should start with an uppercase or lowercase letter (3) It should not have any of these characters: / \ = ' " (4) It should not have spaces .It could have any character that is not mentioned in the list of characters to be avoided (points 3 and 4). Output True if the string forms a valid password and False otherwise.
 
+def is_valid_password(password):
+    if 8 <= len(password) <= 32:
+        if password[0].isalpha():
+            if not any(char in '/\\=\'"' for char in password):
+                if ' ' not in password:
+                    return True  
+    return False
+
+password_input = input("")
+
+result = is_valid_password(password_input)
+print(result)

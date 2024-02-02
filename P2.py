43dbded420582f30e69c3526c7b7e33a79eb7da1
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-02-02 17:32:18 +0530

    New

--- P2.py
+++ P2.py
@@ -5,3 +5,17 @@ if X > 0:
     print("positive")
 else:
     print("negative")
+
+#Accept three positive integers as input and check if they form the sides of a right triangle. Print YES if they form one, and NO if they do not. The input will have three lines, with one integer on each line. The output should be a single line containing one of these two strings: YES or NO.
+
+#DEBUG THE CODE: the template code has errors. Debug the code. 
+x = int(input())
+y = int(input())
+z = int(input())
+if ((x ** 2 + y ** 2 == z ** 2) and
+    (y ** 2 + z ** 2 == x ** 2) and
+    (z ** 2 + x ** 2 == y ** 2)):
+    print("YES")
+else:
+    print("NO")
+

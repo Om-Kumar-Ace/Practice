d2a064fc8bb4e564fa1f4b5e5b3b534c5be0fabf
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-02-21 19:56:55 +0530

    New

---
+++ P4.py
@@ -0,0 +1,10 @@
+#The range of a list of numbers is the difference between the maximum and minimum values in the list.
+
+#Write a function named get_range that accepts a list of real numbers as argument. It should return the range of the list.
+
+#Note
+
+#(1) Avoid using built-in function such as max and min.
+
+#(2) You do not have to accept input from the user or print output to the console. You just have to write the function definition.
+

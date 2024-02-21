8b2c735ad3143b3f5f4b727117fa11000b94c9e7
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-02-21 20:12:14 +0530

    New

--- P4.py
+++ P4.py
@@ -121,3 +121,7 @@ def is_magic(matrix):
         return "NO"
     
     return "YES"
+
+#The transpose of a matrix swaps its rows and columns:
+#Write a function named transpose that accepts a matrix mat as input and returns its transpose.
+#You do not have to accept input from the user or print output to the console. You just have to write the function definition.

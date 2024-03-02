76560922c5c11658763bf64ec889d9a735751b79
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-03-02 19:49:38 +0530

    9

--- Origin/9/script.js
+++ Origin/9/script.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+let random = Math.random()
+let a = prompt("Enter 1st No.")
+let c = prompt("Enter Operation")
+let b = prompt("Enter 2nd No.")
+
+let obj ={
+    "+ ":" *",
+    "/ ":" -",
+    "* ":" /",
+    "- ":" +",
+
+}
+
+if (random >0.10) {
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`) 
+}
+else{
+    c=obj[c]
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+}

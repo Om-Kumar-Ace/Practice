3e20f777e7f3dc9fa28e1e895ebf016e4c78ef6c
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-03-02 20:46:27 +0530

    New

---
+++ P6.py
@@ -0,0 +1,2 @@
+# Write a recursive function named fibo that accepts a positive integer n as argument and returns the nth Fibonacci number. For this problem, F2=1 are the first two Fibonacci numbers.
+

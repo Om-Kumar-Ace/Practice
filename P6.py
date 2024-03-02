8cde9cd4b60b22d7d2ade5f594e82d574ef2d234
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-03-02 20:52:37 +0530

    New

--- P6.py
+++ P6.py
@@ -94,3 +94,32 @@ def factors_upto(n):
     return factors_dict
     
 
+# The scores dataset is a list of dictionaries one of whose entries is given below for your reference:
+
+# {'SeqNo': 1, 'Name': 'Devika', 'Gender': 'F', 'City': 'Bengaluru', 'Mathematics': 85, 'Physics': 100, 'Chemistry': 79, 'Biology': 75, 'Computer Science': 88, 'History': 60, 'Civics': 88, 'Philosophy': 95}
+# Write the following functions:
+
+# (1) group_by_city: accepts the scores_dataset as argument. It should return a dictionary named cities whose keys are names of the cities that the students are from. The value corresponding to a key (city) is the list of names of all students who hail from this city. The order in which names are appended to the list doesn't matter.
+
+# (2) busy_cities: accepts the scores_dataset as argument. It should return a list of cities. Each city in this list has the property that the number of students from this city is greater than or equal to the number of students from every other city in the dataset. Your function should make use of group_by_city. The order in which the cities appended to the list doesn't matter.
+
+# (1) You do not have to accept input from the user or print the output to the console. You just have to write the definition of both the functions.
+
+# (2) Do not try to process the output produced. We randomly sample a few elements from the dictionary or list returned by your function and print that in the desired form.
+
+def group_by_city(scores_dataset):
+    cities = {}
+    for record in scores_dataset:
+        city = record['City']
+        name = record['Name']
+        if city in cities:
+            cities[city].append(name)
+        else:
+            cities[city] = [name]
+    return cities
+
+def busy_cities(scores_dataset):
+    cities = group_by_city(scores_dataset)
+    max_students = max(len(students) for students in cities.values())
+    busy_cities = [city for city, students in cities.items() if len(students) >= max_students]
+    return busy_cities

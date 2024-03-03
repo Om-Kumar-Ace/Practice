a54f63d9b775ad1471b1c5fc2d2cacc89bc6243d
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-03-03 23:05:25 +0530

    10

--- Origin/10/script.js
+++ Origin/10/script.js
@@ -2,12 +2,18 @@ let rand = Math.random();
 let first ,second ,third;
 if (rand <0.33) {
     first ="Crazy"
-
+    second ="Engine"
+    third ="Bros"
 }
 else if (rsnd <0.66 && rand >0.33) {
     first ="Amazing"
-
+    second ="Food"
+    third ="Limited"
+    
 }
 else{
     first ="Fire"
+    second ="Garments"
+    third ="Hub"
+
 }

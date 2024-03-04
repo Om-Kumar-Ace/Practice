0770796cbd350323e7bb2146aa2bdebac130fb26
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-03-04 20:56:35 +0530

    New

--- P7.py
+++ P7.py
@@ -115,3 +115,21 @@ for _ in range(n):
 # True
 
 # Explanation Both 5 and 7 are primes and 7-5=2.
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num == 2:
+        return True
+    if num % 2 == 0:
+        return False
+    for i in range(3, int(num**0.5) + 1, 2):
+        if num % i == 0:
+            return False
+    return True
+
+def twin_primes(p, q):
+    if is_prime(p) and is_prime(q):
+        if abs(p - q) == 2:
+            return True
+    return False

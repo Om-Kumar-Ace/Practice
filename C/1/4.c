58b31c561abbdf36d983c0bfa1009b99a2f42072
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-03-05 21:17:20 +0530

    4

--- C/1/4.c
+++ C/1/4.c
@@ -1,1 +1,12 @@
 // Write a C program to calculate area and circumference of a circle. 
+#include<stdio.h>
+
+int main(){
+    float  r=7 , pi=3.14 ;
+    float area , circum;
+    area = pi*r*r;
+    circum = 2*pi*r;
+    printf("The area of circle is %d",area);
+    printf("The circumference of circle is %d",circum);
+    return 0;
+}

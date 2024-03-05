125591293dc2cc467b6ca2de69ba3a7e00807f43
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-03-05 21:06:35 +0530

    2

--- C/1/2.c
+++ C/1/2.c
@@ -1,2 +1,10 @@
 // Write a C program to add two numbers (2 and 6) and display its sum. 
 
+#include<stdio.h>
+
+int main(){
+    int a = 2 ,b = 6,sum;
+    sum = a+b;
+    printf("The sum is %d",sum);
+    return 0;
+}

85413f3b9f2a5ef41e7628b46e258a66f5069d7c
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-03-05 21:10:38 +0530

    3

--- C/1/3.c
+++ C/1/3.c
@@ -1,1 +1,9 @@
 // Write a C program to multiply two numbers (4 and 5) and display its product. 
+#include<stdio.h>
+
+int main(){
+    int a = 4 ,b = 5,multi;
+    multi = a*b;
+    printf("The sum is %d",multi);
+    return 0;
+}

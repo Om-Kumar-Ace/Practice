4a75010ad267650dfdfa1c461722e1d2bac533ee
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-03-05 21:18:24 +0530

    4

--- C/1/4.c
+++ C/1/4.c
@@ -6,7 +6,7 @@ int main(){
     float area , circum;
     area = pi*r*r;
     circum = 2*pi*r;
-    printf("The area of circle is %d",area);
-    printf("The circumference of circle is %d",circum);
+    printf("The area of circle is %d \n",area);
+    printf("The circumference of circle is %d \n ",circum);
     return 0;
 }

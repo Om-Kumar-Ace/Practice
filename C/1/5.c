8832c2ddff9f68096b39a1686044f6b233a4d90c
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-03-08 19:53:46 +0530

    Revision

--- C/1/5.c
+++ C/1/5.c
@@ -1,2 +1,25 @@
 // Write a C program to perform addition, subtraction, division and multiplication of two numbers.
 
+#include<stdio.h>
+
+int main(){
+    float a,b;
+    printf("Enter the no. ");
+    scanf("%f",&a );
+    printf(" \n Enter the no. ");
+    scanf("%f",&b);
+    float sum ;
+    sum = a+b;
+    printf("The sum of two no %d", sum );
+    float sub ;
+    sub = a-b;
+    printf("The sum of two no %d", sub );
+    float multi ;
+    multi = a*b;
+    printf("The sum of two no %d", multi );
+    float div ;
+    div = a/b;
+    printf("The sum of two no %d", div );
+
+    return 0;
+}

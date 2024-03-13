cd9752f61ae4ac0b2c6a0a39be9beca279d21f0c
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-03-13 22:56:47 +0530

    New

--- P8.py
+++ P8.py
@@ -1,1 +1,7 @@
 # Write a recursive function named reverse that accepts a list L as argument and returns the reversed list.
+def reverse(L):
+    if len(L) <= 1:
+        return L
+    else:
+        return reverse(L[1:]) + [L[0]]
+

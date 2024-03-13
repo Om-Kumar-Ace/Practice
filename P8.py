83dbd0f68416363311f9580c50cacc6419256ce9
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-03-13 23:09:56 +0530

    New

--- P8.py
+++ P8.py
@@ -24,3 +24,26 @@ def linear(P, Q, k):
     return P[0] == k * Q[0] and linear(P[1:], Q[1:], k)
 
 
+# The Collatz function is defined for a positive integer n as follows.
+
+# if is odd
+# if is even
+# f(n)= 3n+12n
+# if n is odd
+# if n is even
+# We consider the repeated application of the Collatz function starting with a given integer n, which results in the following sequence:f(n),f(f(n)),f(f(f(n))),…
+# It is conjectured that no matter which positive integer n you start from, the sequence will always reach 1. For example, if n=10, the sequence is:
+
+# Seq No.	n	f(n)
+# 1	10	5
+# 2	5	16
+# 3	16	8
+# 4	8	4
+# 5	4	2
+# 6	2	1
+# Thus, if you start from n=10, you need to apply the function f six times in order to first reach 1.
+
+# Write a recursive function named collatz that accepts a positive integer n as argument, where 1<n≤32,000, and returns the number of times f has to be applied repeatedly in order to first reach 1.
+
+# You do not have to accept input from the user or print output to the console. You just have to write the function definition.
+

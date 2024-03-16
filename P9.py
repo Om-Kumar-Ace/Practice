9ecfd36dc952bf99a46e0265a860f3e33e81d0d9
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-03-16 22:34:24 +0530

    New

--- P9.py
+++ P9.py
@@ -81,3 +81,15 @@ def uniq(L):
         return uniq(L[1:])
     else:
         return [L[0]] + uniq(L[1:])
+    
+# Write a recursive function named search that accepts the following arguments:
+
+# (1) L: a sorted list of integers
+
+# (2) k: integer
+
+# The function should return True if k is found in the list L, and False otherwise.
+
+# You do not have to accept input from the user or print output to the console. You just have to write the function definition.
+    
+

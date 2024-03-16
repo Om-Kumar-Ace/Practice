d53d2dcb0a424a58faf9c6f76bb906587b2ffbf2
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-03-16 22:29:28 +0530

    New

--- P9.py
+++ P9.py
@@ -49,3 +49,23 @@ def spiral_recursive(left, right, n):
        
         center = (left + right) / 2
         return spiral_recursive(right, center, n - 1)
+
+# Write a recursive function named count that accepts the following arguments:
+
+# L: list of word: a word, could be any string
+# This function should return the number of occurrences of word in L.
+
+# (1) You cannot use the built-in count method for lists in this problem.
+
+# (2) All words will be in lower case.
+
+# (3) Yo do not have to accept input from the user or print the output to the console. You just have to write the definition of both the functions.
+    
+
+def uniq(L):
+    if len(L) == 1:
+        return L
+    if L[0] in L[1:]:
+        return uniq(L[1:])
+    else:
+        return [L[0]] + uniq(L[1:])

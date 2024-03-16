9175414d2dc4545bed42480c5edd7d90952e53b9
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-03-16 22:32:34 +0530

    New

--- P9.py
+++ P9.py
@@ -69,3 +69,8 @@ def uniq(L):
         return uniq(L[1:])
     else:
         return [L[0]] + uniq(L[1:])
+ 
+# Write a recursive function named uniq that accepts a non-empty list L as argument and returns a new list after removing all duplicates from it. Your function must retain the last occurrence of each distinct element in the list.
+
+# You do not have to accept input from the user or print the output to the console. You just have to write the function definition.
+    

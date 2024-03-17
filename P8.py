713d7eb360e5d784bce2883dfbce85b6833d8c1f
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-03-17 20:46:39 +0530

    nNew

--- P8.py
+++ P8.py
@@ -108,3 +108,9 @@ def ancestry(P, present, past):
     else:
         father = P[present]
         return [present] + ancestry(P, father, past)
+    
+
+# Write a recursive function named non_decreasing that accepts a non-empty list L of integers as argument and returns True if the elements are sorted in non-decreasing order from left to right, and False otherwise.
+
+# You do not have to accept input from the user or print the output to the console. You just have to write the function definitio
+    

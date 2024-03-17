2cca1792c65672bb2acafeedb09cdb35f773c0cd
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-03-17 21:09:31 +0530

    New

--- P8.py
+++ P8.py
@@ -118,3 +118,22 @@ def non_decreasing(L):
     if len(L) <= 1:
         return True
     return L[0] <= L[1] and non_decreasing(L[1:])
+
+# You are given two non-empty text files file1 and file2 that have lines respectively. Each file is a collection of ten-digit phone numbers, one number per line. It is also known that 
+
+# The following relations are defined on these two files:
+
+# Subset: file1 is a subset of file2 if the phone number in the 
+# i 
+# th line of file1 is equal to the number in the line of file2, for 
+
+# Equal: file1 is equal to file2 if the phone number in the ℎi th  line of file1 is equal to the number in the 
+
+# Write a function named relation that accepts these two text files as arguments. It should return the string Subset if file1 is a subset of file2. It should return Equal if file1 is equal to file2. If both these conditions are not satisfied, it should return the string No Relation.
+
+# (1) file1 and file2 are string variables that hold the names of the two files. For example, in the first test case, it is file1 = 'public_1_1.txt' and file2 = 'public_1_2.txt'.
+
+# (2) You do not have to accept input from the console or print the output to the console. You just have to write the function definition.
+
+# (3) The strings '9876543210' and '9876543210\n' are not the same, though the phone numbers are the same. So, strip the strings of all new lines using the strip method before checking for equality of two lines across the files.
+

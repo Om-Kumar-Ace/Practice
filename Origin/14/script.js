06bd9efc0bd763405ad432e68bb24b1c98458ad0
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-03-21 23:48:40 +0530

    14

--- Origin/14/script.js
+++ Origin/14/script.js
@@ -15,13 +15,33 @@ const randomDelay = () => {
     })
 }
 
-let text = [ "   Initializing Hacking...",
- "   Readingyour Files...",
- "   Password files Detectd...",
- "   Sending all passwords and ersonal files to server...",
- "   Cleaning up..." ]
+async function main() {
 
- for (const item of text) {
-    addItem(text)
- }
 
+    let t = setInterval(() => {
+        let last = document.body.getElementsByTagName("div");
+        last = last[last.length - 1]
+        if(last.innerHTML.endsWith("...")){
+            last.innerHTML = last.innerHTML.slice(0, last.innerHTML.length-3)
+        }
+        else{
+
+            last.innerHTML = last.innerHTML + "."
+        }
+
+    }, 100);
+
+
+    let text = ["Initialized Hacking now reading your data",
+        "Reading your Files",
+        "Password files Detected",
+        "Sending all passwords and personal files to server",
+        "Cleaning up"]
+
+    for (const item of text) {
+        await addItem(item)
+    }
+
+    await randomDelay()
+    clearInterval(t)}
+main()

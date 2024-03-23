17a6da74968cd181d5bef0889e96f5e714968b9a
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-03-23 23:06:20 +0530

    3

---
+++ C/3/2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+    int  a,b,c,d,e,f,s,o,p=0;
+    
+    printf("Please Enter Over ");
+        scanf("%d",&o);
+
+    printf("Please Enter value in 0 1 2 3 4 6 ");
+    for ( o; o < 2; o++)
+    {
+        printf("Enter run ");
+        scanf("%d",&a);
+        scanf("%d",&b);
+        scanf("%d",&c);
+        scanf("%d",&d);
+        scanf("%d",&e);
+        scanf("%d",&f);
+        s=p+a+b+c+d+e+f;
+
+    float runrate;
+    runrate=s/o;
+    printf("The Runrate is %f",runrate);
+    s=p;}
+    return 0;
+}

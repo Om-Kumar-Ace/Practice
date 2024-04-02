d9ecef991f603f85dbb1f2a2e35ccab156bbee7c
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-04-02 20:35:55 +0530

    10

--- P10.py
+++ P10.py
@@ -110,3 +110,26 @@ class StringManipulation:
 # (5) compute_area: compute the area of the square and assign it to the attribute area.
 # (6) compute_perimeter: compute the perimeter of the square and assign it to the attribute perimeter.
 
+class Shape:
+    def __init__(self, name):
+        self.name = name
+        self.area = None
+        self.perimeter = None
+
+    def display(self):
+        print(f'{self.name} has an area of {self.area} and perimeter of {self.perimeter}')
+
+
+
+class Square(Shape):
+    def __init__(self, side):
+        super().__init__("Square")
+        self.side = side
+        self.compute_area()
+        self.compute_perimeter()
+
+    def compute_area(self):
+        self.area = self.side ** 2
+
+    def compute_perimeter(self):
+        self.perimeter = 4 * self.side

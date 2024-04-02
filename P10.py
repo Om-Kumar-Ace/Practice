8776f5b7789f227cc522ecfd4b3349517eed902a
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-04-02 20:38:05 +0530

    10

--- P10.py
+++ P10.py
@@ -133,3 +133,26 @@ class Square(Shape):
 
     def compute_perimeter(self):
         self.perimeter = 4 * self.side
+
+
+# Create a class Time with the following specification:
+
+# Attributes
+
+# time: int, represents time in seconds
+
+# Methods
+
+# (1) __init__: accept time in seconds as an argument and assign it to the corresponding attribute
+
+# (2) seconds_to_minutes: convert the value of time into minutes and return a string in the format: "<minutes> min <seconds> sec". For example: if the value of the attribute time is 
+# 170
+# 170, this method should return the string "2 min 50 sec"
+
+# (3) seconds_to_hours: convert the value of time into hours and return a string in the format: "<hours> hrs <minutes> min <seconds> sec". For example: if the value of the attribute time is 
+# 10890
+# 10890, this method should return the string "3 hrs 1 min 30 sec"
+
+# (4) seconds_to_days: convert the value of time into days and return a string in the format: "<days> days <hours> hrs <minutes> min <seconds> sec". For example: if the value of the attribute time is 
+# 86460
+# 86460, this method should return the string "1 days 0 hrs 1 min 0 sec"

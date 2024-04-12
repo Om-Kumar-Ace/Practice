a265810120896bad945d25b9dac0e0f30986ca6d
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-04-12 22:24:19 +0530

    10

--- P8.py
+++ P8.py
@@ -156,3 +156,7 @@ def relation(file1, file2):
 
 # Write a function named read_file that accepts a text file named filename as argument. Within the function, read the file and print each line of the file on a separate line in the console. You shouldn't print any extra characters at the end of a line. There shouldn't be an empty line between any two consecutive lines.
 
+def read_file(filename):
+    with open(filename, 'r') as file:
+        for line in file:
+            print(line.rstrip())

799bf1ef1a1d4e6bc34241db547b07fee1aa12b0
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-04-15 20:29:17 +0530

    6

---
+++ C/4/6.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+    int n ;
+    printf("Enter the no.");
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k <  n - i - 1 ; k++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            printf("*");
+        }
+        for (int l = 0; l <= i; l++)
+        {
+            printf("*");
+        }
+        printf("\n");
+
+    }
+    return 0;
+}

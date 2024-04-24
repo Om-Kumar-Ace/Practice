1a57c18e0a8e5d130ee920bda8eebfb30244f509
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-04-25 01:04:49 +0530

    4

--- C/5/4.c
+++ C/5/4.c
@@ -1,27 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void main() {
-    int arr[] = {10,11,8,10,9,14,11,14,3,8,10,7,8,6,7,5,6};
-    int b[17] = {0}; 
-    int a[17]; 
-    int n = 0; 
+void main()
+{
+    int arr[] = {10, 11, 8, 10, 9, 14, 11, 14, 3, 8, 10, 7, 8, 6, 7, 5, 6};
+    int b[17] = {0};
+    int a[17];
+    int n = 0;
 
-    for (int i = 0; i < 17; i++) {
+    for (int i = 0; i < 17; i++)
+    {
         int c = 0;
         int d = 0;
-        for (int k = 0; k < n; k++) {
-            if (arr[i] == a[k]) {
+        for (int k = 0; k < n; k++)
+        {
+            if (arr[i] == a[k])
+            {
                 d = 1;
                 break;
             }
         }
-        if (d) {
+        if (!d)
+        {
 
-            for (int j = 0; j < 17; j++) {
-                if (arr[i] == arr[j]) {
+            for (int j = 0; j < 17; j++)
+            {
+                if (arr[i] == arr[j])
+                {
                     c++;
-
-                }   
+                }
             }
             a[n] = arr[i];
             b[n] = c;
@@ -29,8 +35,17 @@ void main() {
         }
     }
 
-    for (int i = 0; i < n; i++) {
-        printf("%d occurs %d times\n", a[i], b[i]);
-    }
+    for (int i = 0; i < n; i++)
+    {
+        printf(" %d occurs %d times \n", a[i], b[i]);
 
+        for (int j = 0; j < 17; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                printf(" At position %d ", j + 1);
+            }
+        }
+        printf("\n");
+    }
 }

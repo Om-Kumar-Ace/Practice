ae6959aa000089a73a49a836169c3cb12b0582aa
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-05-07 15:37:56 +0530

    1

---
+++ C/8/1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10] = {0};
+    int n = 10; 
+
+    printf("Enter array of 10 no. : \n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d ", &arr[i]);
+    }
+    printf("Original array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}

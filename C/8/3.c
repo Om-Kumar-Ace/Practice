0753d261d7bf031307e4b6c9d942d7e055a5a8f8
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-05-11 19:14:09 +0530

    3

--- C/8/3.c
+++ C/8/3.c
@@ -3,13 +3,14 @@
 void main(){
     int arr[]={5,7,6,2,4,11};
     int n=6;
-
     for (int i = 1; i < n; i++)
     {
         int temp=arr[i];
         int j = i - 1;
-        for (; j >= 0 && arr[j] > temp; j--)
+        for (; j >= 0; j--)
         {
+            if (arr[j] <= temp)
+                break;
             arr[j+1]=arr[j];
         }
         arr[j+1] = temp;

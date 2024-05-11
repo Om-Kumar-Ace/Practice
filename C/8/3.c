f642d317cdc841bfc564edc50855757669358472
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-05-11 18:51:56 +0530

    3

---
+++ C/8/3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void main(){
+    int arr[]={5,7,6,2,4,11};
+    int n=6;
+
+    for (int i = 1; i < n; i++)
+    {
+        int temp=arr[i];
+        int j = i - 1;
+        for (; j >= 0 && arr[j] > temp; j--)
+        {
+            arr[j+1]=arr[j];
+        }
+        arr[j+1] = temp;
+    }
+
+    printf("Sorted array: ");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}

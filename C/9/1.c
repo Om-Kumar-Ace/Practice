bd6530b82d7d518c1af06e73553d25dc5a53c35c
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-05-14 15:49:23 +0530

    1

---
+++ C/9/1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main() {
+    char str[100];
+    int count = 0;
+    int i;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ') 
+            count++;
+        
+    }
+    printf("Number of spaces in the string: %d\n", count);
+
+}

ea543f5d7a52a5e4fabe92bcf28a8ccb5f505707
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-05-14 16:15:02 +0530

    2

--- C/9/2.c
+++ C/9/2.c
@@ -6,17 +6,19 @@ void main() {
     printf("Enter the size of the array: ");
     scanf("%d", &size);
 
-    int arr[100]={0};
+    int arr[100] = {0};
+    int temp[100] = {0};
 
     printf("Enter %d elements of the array: \n", size);
     for (int i = 0; i < size; i++) 
         scanf("%d", &arr[i]);
     
+    for (int i = 0; i < size; i++) 
+        temp[i] = arr[size - i - 1];
 
     printf("The reverse of the array is: ");
-    for (int i = size - 1; i >= 0; i--) 
-        printf("%d ", arr[i]);
+    for (int i = 0; i < size; i++) 
+        printf("%d ", temp[i]);
     
     printf("\n");
-
 }

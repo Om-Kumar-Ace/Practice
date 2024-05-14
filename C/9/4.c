c1d217f6ca43ab01effc8b713bc57587ee863a81
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-05-15 01:07:00 +0530

    4

---
+++ C/9/4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+int main() {
+    int arr[100];
+    int size, i, num;
+
+    printf("Enter size of array (max 100): ");
+    scanf("%d", &size);
+
+    if (size >= 100) {
+        printf("Error: Size exceeds maximum limit.");
+        return 1;
+    }
+
+    printf("Enter elements of array:\n");
+    for (i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the number to insert at the end: ");
+    scanf("%d", &num);
+
+    arr[size] = num; 
+
+    size++; 
+
+    printf("Array after insertion at the end:\n");
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}

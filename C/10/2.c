e28132c518469da376ea661213bf7a0cb09a8891
Om-Kumar-Ace <om2005kumar001@gmail.com>
2024-05-16 20:01:04 +0530

    2

--- C/10/2.c
+++ C/10/2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main(){
-    int n;
+    int n,m=0;
     printf("Enter no of rows :");
     scanf("%d",&n);
     n++;
@@ -14,6 +14,20 @@ int main(){
         for (int j = i-1; j > 0 ; j--)
             printf(" %d",j);
         printf("\n");
+    }
+        printf("\n");
+        printf("***********************************************************\n");
+        printf("\n");
+    for (int i = n; i > 0; i--)
+    {
+        for (int j = 1; j <=i ; j++)
+            printf(" %d",j);
+        for (int k = 1; k <=n-i ; k++)
+            printf("    ");
+        for (int j = i; j > 0 ; j--)
+            printf(" %d",j);
+        printf("\n");
+        m+2;
     }
     return 0;
 }

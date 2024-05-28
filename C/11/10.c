6cea617de485be5de02c7ddb30ee818f25a40a40
Om Kumar <om2005kumar001@gmail.com>
2024-05-28 16:00:25 +0530

    10

---
+++ C/11/10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int factorial(int x);
+
+int main()
+{
+    int a;
+    printf("Enter the number : ");
+    scanf("%d", &a);
+    printf("\nTHE VALUE OF FACTORIAL OF %d is %d  : ", a, factorial(a));
+
+    return 0;
+}
+
+int factorial(int x)
+{
+    printf("Calling factorial %d\n", x);
+    if (x == 1 || x == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return x * factorial(x-1);
+    }
+}

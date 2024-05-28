6a7657542882400d5b1b69e3b69c56c38e933929
Om Kumar <om2005kumar001@gmail.com>
2024-05-28 16:04:03 +0530

    10

--- C/11/10.c
+++ C/11/10.c
@@ -16,11 +16,7 @@ int factorial(int x)
 {
     printf("Calling factorial %d\n", x);
     if (x == 1 || x == 0)
-    {
         return 1;
-    }
     else
-    {
         return x * factorial(x-1);
-    }
 }

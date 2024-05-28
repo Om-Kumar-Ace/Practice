72182d6d98671e01c72552b5d2da05110e6153a0
Om Kumar <om2005kumar001@gmail.com>
2024-05-28 16:30:58 +0530

    10

--- C/11/10.c
+++ C/11/10.c
@@ -7,14 +7,13 @@ int main()
     int a;
     printf("Enter the number : ");
     scanf("%d", &a);
-    printf("\nTHE VALUE OF FACTORIAL OF %d is %d  : ", a, factorial(a));
+    printf("\nTHE VALUE OF FACTORIAL OF %d is %d .", a, factorial(a));
 
     return 0;
 }
 
 int factorial(int x)
 {
-    printf("Calling factorial %d\n", x);
     if (x == 1 || x == 0)
         return 1;
     else

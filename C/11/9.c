b39867cffe851ca5de5fdfd37f3dc0a0f81819aa
Om Kumar <om2005kumar001@gmail.com>
2024-05-28 15:59:35 +0530

    9

---
+++ C/11/9.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void swapByValue(int a, int b);
+void swapByReference(int *a, int *b);
+
+int main() {
+    int x, y;
+
+    printf("Enter the first integer: ");
+    scanf("%d", &x);
+    printf("Enter the second integer: ");
+    scanf("%d", &y);
+
+    printf("\nBefore swapping (by value): x = %d, y = %d\n", x, y);
+    swapByValue(x, y);
+    printf("After swapping (by value): x = %d, y = %d\n", x, y);
+
+    printf("\nBefore swapping (by reference): x = %d, y = %d\n", x, y);
+    swapByReference(&x, &y);
+    printf("After swapping (by reference): x = %d, y = %d\n", x, y);
+
+    return 0;
+}
+
+void swapByValue(int a, int b) {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    printf("Inside swapByValue function: a = %d, b = %d\n", a, b);
+}
+
+void swapByReference(int *a, int *b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}

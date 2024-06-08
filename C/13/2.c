b62e78e45d22b20eaa28beffbe5ac791540ac995
Om Kumar <om2005kumar001@gmail.com>
2024-06-08 18:28:14 +0530

    2

--- C/13/2.c
+++ C/13/2.c
@@ -1,21 +1,39 @@
-#include<conio.h>
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h>
 
-int fact(int);
-
-void main()
-{
-    int f, r;
-    printf("Enter any number: ");
-    scanf("%d", &f);
-    r = fact(f);
-    printf("Factorial of %d is %d", f, r);
+double calculateMean(int data[], int n) {
+    double sum = 0.0;
+    for (int i = 0; i < n; ++i) {
+        sum += data[i];
+    }
+    return sum / n;
 }
 
-int fact(int f)
-{
-    if (f == 0)
-        return 1;
-    else
-        return (f * fact(f - 1));
+double calculateStandardDeviation(int data[], int n) {
+    double mean = calculateMean(data, n);
+    double sumSquaredDifferences = 0.0;
+
+    for (int i = 0; i < n; ++i) {
+        sumSquaredDifferences += pow(data[i] - mean, 2);
+    }
+
+    return sqrt(sumSquaredDifferences / n);
+}
+
+int main() {
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int data[n];
+    printf("Enter the elements:\n");
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &data[i]);
+    }
+
+    double stddev = calculateStandardDeviation(data, n);
+    printf("Standard Deviation = %.2lf\n", stddev);
+
+    return 0;
 }

f3c36656517fcace4b3ed41b94ee4253dbab7f78
Om Kumar <om2005kumar001@gmail.com>
2024-07-16 22:34:26 +0530

    if-else (Decision Making)

---
+++ C++/Week 4/Day_4_3.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Solution {
+  public:
+    string compareNM(int n, int m){
+        if(n<m){
+            return "lesser";
+        }
+        else if (m<n){
+            return "greater";
+        }
+        else {
+            return "equal";
+        }
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,m ; cin>>n>>m;
+        Solution obj;
+        cout<<obj.compareNM(n,m)<<"\n";
+    }
+    return 0;
+}

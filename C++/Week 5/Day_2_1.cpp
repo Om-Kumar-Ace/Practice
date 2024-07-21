6403a1671d3d2c5d27daa36c5efe6a35e607d56a
Om Kumar <om2005kumar001@gmail.com>
2024-07-21 18:50:38 +0530

    Build a matrix

---
+++ C++/Week 5/Day_2_1.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    bool buildGraph(vector<vector<int>>& arr, int n, vector<int>& data) {
+        vector<vector<int>> adj(n + 1);
+        vector<int> indeg(n + 1, 0);
+        for(auto& curr: arr) {
+            adj[curr[0]].push_back(curr[1]);
+            indeg[curr[1]]++;
+        }
+        queue<int> q;
+        for(int i = 1; i <= n; i++) {
+            if(indeg[i] == 0) {
+                q.push(i);
+            }
+        }
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            data.push_back(node);
+            for(auto& x: adj[node]) {
+                if(--indeg[x] == 0) {
+                    q.push(x);
+                }
+            }
+        }
+        return data.size() == n;
+    }
+
+    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        vector<int> topoRow, topoCol;
+        if(!buildGraph(rowConditions, k, topoRow) || !buildGraph(colConditions, k, topoCol)) {
+            return {};
+        }
+
+        int row[k + 1], col[k + 1];
+        for(int i = 0; i < k; i++) {
+            row[topoRow[i]] = i;
+            col[topoCol[i]] = i;
+        }
+
+        vector<vector<int>> arr(k, vector<int>(k, 0));
+        for(int i = 1; i <= k; i++) {
+            arr[row[i]][col[i]] = i;
+        }
+        return arr;
+    }
+};
+
+int main() {
+    Solution solution;
+    int k = 3;
+    vector<vector<int>> rowConditions = {{1, 2}, {3, 2}};
+    vector<vector<int>> colConditions = {{2, 1}, {3, 2}};
+    
+    vector<vector<int>> result = solution.buildMatrix(k, rowConditions, colConditions);
+    for(const auto& row : result) {
+        for(int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}

2a23af67b521c6acaecacdc29de910b711584dc9
Om Kumar <om2005kumar001@gmail.com>
2024-08-08 18:15:02 +0530

    Sum Tree

--- C++/Week 7/Day_6_3.cpp
+++ C++/Week 7/Day_6_3.cpp
@@ -1,15 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node {
+struct Node
+{
     int data;
-    struct Node* left;
-    struct Node* right;
+    struct Node *left;
+    struct Node *right;
 };
 
 // Utility function to create a new Tree Node
-Node* newNode(int val) {
-    Node* temp = new Node;
+Node *newNode(int val)
+{
+    Node *temp = new Node;
     temp->data = val;
     temp->left = NULL;
     temp->right = NULL;
@@ -18,7 +20,8 @@ Node* newNode(int val) {
 }
 
 // Function to Build Tree
-Node* buildTree(string str) {
+Node *buildTree(string str)
+{
     // Corner Case
     if (str.length() == 0 || str[0] == 'N')
         return NULL;
@@ -32,25 +35,27 @@ Node* buildTree(string str) {
         ip.push_back(str);
 
     // Create the root of the tree
-    Node* root = newNode(stoi(ip[0]));
+    Node *root = newNode(stoi(ip[0]));
 
     // Push the root to the queue
-    queue<Node*> queue;
+    queue<Node *> queue;
     queue.push(root);
 
     // Starting from the second element
     int i = 1;
-    while (!queue.empty() && i < ip.size()) {
+    while (!queue.empty() && i < ip.size())
+    {
 
         // Get and remove the front of the queue
-        Node* currNode = queue.front();
+        Node *currNode = queue.front();
         queue.pop();
 
         // Get the current node's value from the string
         string currVal = ip[i];
 
         // If the left child is not null
-        if (currVal != "N") {
+        if (currVal != "N")
+        {
 
             // Create the left child for the current node
             currNode->left = newNode(stoi(currVal));
@@ -66,7 +71,8 @@ Node* buildTree(string str) {
         currVal = ip[i];
 
         // If the right child is not null
-        if (currVal != "N") {
+        if (currVal != "N")
+        {
 
             // Create the right child for the current node
             currNode->right = newNode(stoi(currVal));
@@ -81,24 +87,21 @@ Node* buildTree(string str) {
 }
 
 
-// } Driver Code Ends
-/*  Tree node
-struct Node
+class Solution
 {
-    int data;
-    Node* left, * right;
-}; */
-
-// Should return true if tree is Sum Tree, else false
-class Solution {
-  public:
-   int sum(Node *root){
-      if(root==NULL) return 0;
-      return root->data+sum(root->left)+sum(root->right);
-  }
-    bool isSumTree(Node* root) {
-        if(root==NULL || root->left==NULL && root->right==NULL) return true;
-        if(root->data==sum(root->left)+sum(root->right) && isSumTree(root->left) && isSumTree(root->right)){
+public:
+    int sum(Node *root)
+    {
+        if (root == NULL)
+            return 0;
+        return root->data + sum(root->left) + sum(root->right);
+    }
+    bool isSumTree(Node *root)
+    {
+        if (root == NULL || root->left == NULL && root->right == NULL)
+            return true;
+        if (root->data == sum(root->left) + sum(root->right) && isSumTree(root->left) && isSumTree(root->right))
+        {
             return true;
         }
         return false;
@@ -107,16 +110,18 @@ class Solution {
 
 //{ Driver Code Starts.
 
-int main() {
+int main()
+{
 
     int t;
     scanf("%d ", &t);
-    while (t--) {
+    while (t--)
+    {
         string s;
 
         getline(cin, s);
 
-        Node* root = buildTree(s);
+        Node *root = buildTree(s);
         Solution ob;
         if (ob.isSumTree(root))
             cout << "true" << endl;

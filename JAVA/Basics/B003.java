b1e3cd203286785cfd8f1b66452f01097a4ce3bf
Om Kumar <om2005kumar001@gmail.com>
2024-08-25 00:20:27 +0530

    Intro to JAVA 3

---
+++ JAVA/Basics/B003.java
@@ -0,0 +1,18 @@
+
+
+public class B003 {
+    public static void main(String[] args) {
+        
+        System.out.println("Enter Salary:");
+        // Scanner  salary = new Scanner(System.in);
+        float salary =200000;
+        if (salary<2000.00) {
+            salary=salary+100;
+        }
+        else{
+            
+            salary=salary+200;
+        }
+        System.err.println(salary);
+    }
+}

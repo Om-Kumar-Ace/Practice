e3008c2c56300d0e06d3b55ab2637ad49e92baa8
Om Kumar <om2005kumar001@gmail.com>
2024-10-02 18:52:54 +0530

    Rank Transform of an Array

---
+++ C++/Pratice/116.cpp
@@ -0,0 +1,40 @@
+#include <execution>
+
+class Solution {
+public:
+    constexpr inline vector<int> arrayRankTransform(vector<int>& arr) {
+        if (arr.empty()) {
+            return {};
+        }
+
+        const int n = arr.size();
+        vector<pair<int, int>> result(n);        
+        transform(// std::execution::par_unseq,
+            arr.begin(), arr.end(), result.begin(),
+            [i = 0](int value) mutable { return make_pair(value, i++); });
+
+        sort(execution::par_unseq,
+            result.begin(), result.end(),
+            [](const pair<int, int>& p1, const pair<int, int>& p2){
+                return p1.first < p2.first;
+            });
+
+        int rank = 1;
+        for (int i = 0; i < result.size() - 1; ++i) {
+            arr[result[i].second] = rank;
+            if (result[i].first != result[i + 1].first) {
+                ++rank;
+            }
+        }
+
+        arr[result.back().second] = rank;
+        return arr;        
+    }
+};
+
+static const auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();

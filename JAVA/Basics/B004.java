beff1bea2709d1ace028132dbb0c4d7ad5d62048
Om Kumar <om2005kumar001@gmail.com>
2024-10-05 01:44:29 +0530

    Fun

--- JAVA/Basics/B004.java
+++ JAVA/Basics/B004.java
@@ -4,21 +4,14 @@ public class B004 {
         Scanner in =new Scanner(System.in);
         String fruit = in.next();
         switch (fruit) {
-            case "Mango":
-                System.out.println("King of fruits");
-                break;
-            case "Apple":
-                System.out.println("Red fruit");
-                break;
-            case "Orange":
-                System.out.println("Orange");
-                break;
-            case "Grape":
-                System.out.println("Many in One");
-                break;
-            default:
+            case "Mango" -> System.out.println("King of fruits");
+            case "Apple" -> System.out.println("Red fruit");
+            case "Orange" -> System.out.println("Orange");
+            case "Grape" -> System.out.println("Many in One");
+            default -> {
                 System.out.println("Please !! Enter a valid Fruit");
                 throw new AssertionError();
+            }
         }
     }
 }
